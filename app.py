8a60862ffef5659671ef955ed253690e63f49758
Erik Seyti <erikseyti@gmail.com>
2020-12-19 17:42:08 -0300

    Criado metodo para conseguir numero aleatorio por meio de um intervalo

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import random
 app = Flask(__name__)
 
 @app.route('/', methods=['GET','POST'])
 def hello_world():
-    return render_template('paginaInicial.html')
+    if request.method == "GET":
+        return render_template('paginaInicial.html')
+    elif request.method == "POST":
+        print(request.form)
+        firstNumber = int(request.form['firstNumber'])
+        lastNumber = int(request.form['lastNumber'])
+
+        randomNumber = random.randrange(firstNumber, lastNumber)
+        print(randomNumber)
+
+
+        return render_template('paginaInicial.html', randomNumber=randomNumber)
